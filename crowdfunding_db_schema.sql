00a0b7151e5cc398220a8b5eafd82d613f78baa9
Deogratius Nteza <nteza.deogratius@gmail.com>
2024-07-29 19:19:30 -0400

    Crowd Funding ETL

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,3 +1,6 @@
+-- Drop the Contacts table if it exists
+DROP TABLE IF EXISTS Contacts CASCADE;
+
 -- Create the Contacts table
 CREATE TABLE Contacts (
     contact_id SERIAL PRIMARY KEY,
@@ -6,17 +9,26 @@ CREATE TABLE Contacts (
     email VARCHAR(55) NOT NULL UNIQUE
 );
 
+-- Drop the Category table if it exists
+DROP TABLE IF EXISTS Category CASCADE;
+
 -- Create the Category table
 CREATE TABLE Category (
-    category_id SERIAL PRIMARY KEY,
+    category_id VARCHAR(5) PRIMARY KEY,
     category_name VARCHAR(15) NOT NULL
 );
 
+-- Drop the Subcategory table if it exists
+DROP TABLE IF EXISTS Subcategory CASCADE;
+
 -- Create the Subcategory table
 CREATE TABLE Subcategory (
-    subcategory_id SERIAL PRIMARY KEY,
+    subcategory_id VARCHAR(10) PRIMARY KEY,
     subcategory_name VARCHAR(25) NOT NULL
 );
+
+-- Drop the Campaign table if it exists
+DROP TABLE IF EXISTS Campaign CASCADE;
 
 -- Create the Campaign table
 CREATE TABLE Campaign (
@@ -32,8 +44,8 @@ CREATE TABLE Campaign (
     currency VARCHAR(3) NOT NULL,
     launch_date DATE NOT NULL,
     end_date DATE NOT NULL,
-    category_id INTEGER NOT NULL,
-    subcategory_id INTEGER NOT NULL,
+    category_id VARCHAR(5) NOT NULL,
+    subcategory_id VARCHAR(10) NOT NULL,
     FOREIGN KEY (contact_id) REFERENCES Contacts(contact_id),
     FOREIGN KEY (category_id) REFERENCES Category(category_id),
     FOREIGN KEY (subcategory_id) REFERENCES Subcategory(subcategory_id)
--- import_data.sql
+++ import_data.sql
@@ -1,5 +1,28 @@
-COPY contacts(contact_id, first_name, last_name, email) FROM 'G:\GIT-HUB\crowdfunding_ETL\Resources\contacts.csv' WITH (FORMAT CSV, HEADER true);
-COPY category(category_id, category_name) FROM 'G:\GIT-HUB\crowdfunding_ETL\Resources\category.csv' WITH (FORMAT CSV, HEADER true);
-COPY subcategory(subcategory_id, subcategory_name) FROM 'G:\GIT-HUB\crowdfunding_ETL\Resources\subcategory.csv' WITH (FORMAT CSV, HEADER true);
-COPY campaign(cf_id, contact_id, company_name, description, goal, pledged, outcome, backers_count, country, currency, launch_date, end_date, category_id, subcategory_id) FROM 'G:\GIT-HUB\crowdfunding_ETL\Resources\campaign.csv' WITH (FORMAT CSV, HEADER true);
+COPY contacts(contact_id, first_name, last_name, email) FROM '\Resources\contacts.csv' WITH (FORMAT CSV, HEADER true);
+COPY category(category_id, category_name) FROM '\Resources\category.csv' WITH (FORMAT CSV, HEADER true);
+COPY subcategory(subcategory_id, subcategory_name) FROM '\Resources\subcategory.csv' WITH (FORMAT CSV, HEADER true);
+COPY campaign(cf_id, contact_id, company_name, description, goal, pledged, outcome, backers_count, country, currency, 
+launch_date, end_date, category_id, subcategory_id) FROM '\Resources\campaign.csv' WITH (FORMAT CSV, HEADER true);
 
+-- Select Querry to check if data is loaded in all tables
+SELECT 
+    c.first_name,
+    c.last_name,
+    c.email,
+    cat.category_name,
+    sub.subcategory_name,
+    camp.cf_id,
+    camp.company_name,
+    camp.description,
+    camp.goal,
+    camp.pledged,
+    camp.outcome,
+    camp.backers_count,
+    camp.country,
+    camp.currency,
+    camp.launch_date,
+    camp.end_date
+FROM Contacts c
+JOIN Campaign camp ON c.contact_id = camp.contact_id
+JOIN Category cat ON camp.category_id = cat.category_id
+JOIN Subcategory sub ON camp.subcategory_id = sub.subcategory_id;
